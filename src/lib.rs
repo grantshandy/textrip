0afb24fcf030a89fa47faebe4147a7bbde094eb3
grantshandy <grantshandy@gmail.com>
2021-06-19 13:13:01 -0600

    fix it up

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,10 @@
 extern crate image;
+// extern crate js_sys;
 extern crate wasm_bindgen;
 extern crate web_sys;
-// extern crate js_sys;
 
 use image::io::Reader as ImageReader;
 // use js_sys::Uint8Array;
-use web_sys::{HtmlElement, HtmlImageElement};
 use std::io::Cursor;
 use wasm_bindgen::prelude::*;
 
@@ -19,23 +18,33 @@ extern "C" {
 
 #[wasm_bindgen]
 pub fn run(value: &[u8]) {
-    let image = ImageReader::new(Cursor::new(value)).with_guessed_format().expect("can't get imagereader from the image");
+    let image = ImageReader::new(Cursor::new(value))
+        .with_guessed_format()
+        .expect("can't get imagereader from the image");
+        // .decode()
+        // .expect("couldn't decode");
 
-    let (x, y) = image.into_dimensions().expect("can't get image dimensions");
-    let message = format!("({}, {})", x, y);
+    // let image = match image.decode() {
+    //     Ok(data) => data,
+    //     Err(e) => {
+    //         log(&e.to_string());
+    //         std::process::exit(1);
+    //     }
+    // };
+
+    let (x, y) = image.into_dimensions().unwrap();
+    let message = format!("resolution: ({}, {})", x, y);
 
     log(&message);
 
     let window = web_sys::window().expect("no global `window` exists");
     let document = window.document().expect("should have a document on window");
     let body = document.body().expect("document should have a body");
-    
-    let resolution = body.children().named_item("resolution").expect("couldn't find resolution");
-    let preview = body.children().named_item("preview").expect("couldn't find preview");
+
+    let resolution = body
+        .children()
+        .named_item("resolution")
+        .expect("couldn't find resolution");
 
     resolution.set_text_content(Some(&message));
-
-    // let image = image.decode().expect("can't decode").grayscale();
-
-    // return Uint8Array::from(image.as_bytes());
 }
