17218aca37ad5ec3e57b175e1aa3ee2335405c80
grantshandy <grantshandy@gmail.com>
2021-06-18 18:10:29 -0600

    format

--- src/lib.rs
+++ src/lib.rs
@@ -2,16 +2,9 @@ extern crate image;
 extern crate wasm_bindgen;
 
 // use std::fs::File;
-use std::io::Cursor;
 use image::io::Reader as ImageReader;
+use std::io::Cursor;
 use wasm_bindgen::prelude::*;
-
-// #[macro_use]
-// extern crate stdweb;
-
-// use stdweb::*;
-// use stdweb::web::FileReader;
-// use stdweb::web::FileReaderResult;
 
 #[wasm_bindgen]
 extern "C" {
@@ -25,15 +18,14 @@ extern "C" {
 pub fn resolution(value: &[u8]) -> String {
     log(&format!("bytes: {}", &value.len()));
 
-    let image = match ImageReader::new(Cursor::new(value))
-        .with_guessed_format() {
-            Ok(data) => data,
-            Err(e) => {
-                let e = format!("image-rs error: {}", e);
-                error(&e);
-                return e;
-            }
-        };
+    let image = match ImageReader::new(Cursor::new(value)).with_guessed_format() {
+        Ok(data) => data,
+        Err(e) => {
+            let e = format!("image-rs error: {}", e);
+            error(&e);
+            return e;
+        }
+    };
 
     let (x, y) = match image.into_dimensions() {
         Ok(data) => data,
