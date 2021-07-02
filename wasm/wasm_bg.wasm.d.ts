34d733062f54b8236760fa58fcf4fa859d3da7f4
grantshandy <grantshandy@gmail.com>
2021-07-02 12:35:16 -0600

    print_points

--- wasm/wasm_bg.wasm.d.ts
+++ wasm/wasm_bg.wasm.d.ts
@@ -3,10 +3,12 @@
 export const memory: WebAssembly.Memory;
 export function run(a: number, b: number): number;
 export function get_dimensions(a: number, b: number): number;
+export function print_points(a: number): void;
 export function __wbg_resolution_free(a: number): void;
 export function __wbg_get_resolution_width(a: number): number;
 export function __wbg_set_resolution_width(a: number, b: number): void;
 export function __wbg_get_resolution_height(a: number): number;
 export function __wbg_set_resolution_height(a: number, b: number): void;
 export function __wbindgen_malloc(a: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number): number;
 export function __wbindgen_exn_store(a: number): void;
